8adb44c94a4bd96164561b09877594f39c002087
Brenton Ashworth <brenton@thinkrelevance.com>
2014-03-22 08:55:26 -0400

    Update Time and UUID decoders to handle tagged maps

--- src/main/java/com/cognitect/transit/impl/decode/TimeDecoder.java
+++ src/main/java/com/cognitect/transit/impl/decode/TimeDecoder.java
@@ -4,18 +4,25 @@ import com.cognitect.transit.Decoder;
 import com.cognitect.transit.impl.JsonParser;
 
 import java.text.ParseException;
+import java.util.Date;
 
 public class TimeDecoder implements Decoder {
 
     @Override
     public Object decode(Object encodedVal) {
 
-        try {
-            return JsonParser.dateTimeFormat.parse((String)encodedVal);
-        } catch(ParseException e) {
-            // TODO: What should happen here
-            System.out.println("WARNING: Could not decode time: " + encodedVal);
-            return "~t" + encodedVal;
+        if(encodedVal instanceof String) {
+            try {
+                return JsonParser.dateTimeFormat.parse((String)encodedVal);
+            } catch(ParseException e) {
+                // TODO: What should happen here?
+                System.out.println("WARNING: Could not decode time: " + encodedVal);
+                return "~t" + encodedVal;
+            }
         }
+        else {
+            return new Date((Long)encodedVal);
+        }
+
     }
 }
--- src/main/java/com/cognitect/transit/impl/decode/UUIDDecoder.java
+++ src/main/java/com/cognitect/transit/impl/decode/UUIDDecoder.java
@@ -2,6 +2,7 @@ package com.cognitect.transit.impl.decode;
 
 import com.cognitect.transit.Decoder;
 
+import java.util.List;
 import java.util.UUID;
 
 public class UUIDDecoder implements Decoder {
@@ -9,6 +10,12 @@ public class UUIDDecoder implements Decoder {
     @Override
     public Object decode(Object encodedVal) {
 
-        return UUID.fromString((String)encodedVal);
+        if(encodedVal instanceof String) {
+            return UUID.fromString((String)encodedVal);
+        }
+        else {
+            List<Long> l = (List<Long>)encodedVal;
+            return new UUID(l.get(0), l.get(1));
+        }
     }
 }
