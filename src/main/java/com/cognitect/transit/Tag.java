ce3076d71d4096e0b9fd691efdd09bfff41ef5bd
Brenton Ashworth <brenton@thinkrelevance.com>
2014-03-24 11:40:22 -0400

    Add tags

--- src/main/java/com/cognitect/transit/Tag.java
+++ src/main/java/com/cognitect/transit/Tag.java
@@ -2,9 +2,19 @@ package com.cognitect.transit;
 
 public enum Tag {
 
-    NULL ("_", true),
-    STRING ("s", true),
-    BOOLEAN ("?", true);
+    NULL        ("_", true),
+    STRING      ("s", true),
+    BOOLEAN     ("?", true),
+    INTEGER     ("i", true),
+    DOUBLE      ("d", true),
+    BIG_DECIMAL ("f", true),
+    CHARACTER   ("c", true),
+    KEYWORD     (":", true),
+    SYMBOL      ("$", true),
+    BINARY      ("b", true),
+    UUID        ("u", true),
+    URI         ("r", true),
+    MAP         ("map", false);
 
     private final String tag;
     public final boolean allowedMapKey;
