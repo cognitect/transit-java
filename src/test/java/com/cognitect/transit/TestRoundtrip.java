265a9f725f4497cab87f75a2415cd6ce33aa49f3
Brenton Ashworth <brenton@thinkrelevance.com>
2014-04-26 11:06:46 -0400

    Use args to create the desired instance of transit

--- src/test/java/com/cognitect/transit/TestRoundtrip.java
+++ src/test/java/com/cognitect/transit/TestRoundtrip.java
@@ -7,9 +7,19 @@ public class TestRoundtrip {
 
     public static void main(String [] args) throws Exception {
 
-        // TODO: use argument to set up json or msgpack
-        Reader reader = Reader.getJsonInstance(System.in, Reader.defaultDecoders());
-        Writer writer = Writer.getJsonInstance(System.out, Writer.defaultHandlers());
+        String encoding = args[0];
+
+        Reader reader;
+        Writer writer;
+
+        if(encoding.equals("msgpack")) {
+            reader = Reader.getMsgpackInstance(System.in, Reader.defaultDecoders());
+            writer = Writer.getMsgpackInstance(System.out, Writer.defaultHandlers());
+        }
+        else {
+            reader = Reader.getJsonInstance(System.in, Reader.defaultDecoders());
+            writer = Writer.getJsonInstance(System.out, Writer.defaultHandlers());
+        }
 
         try {
             while(true) {
