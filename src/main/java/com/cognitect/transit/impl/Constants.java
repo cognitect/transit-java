6cb7cdeba8c8c1ccdccbdf59ab669489fbf87b8b
fogus <mefogus@gmail.com>
2014-05-09 12:31:55 -0400

    Pulled out tag constants into a separate class

---
+++ src/main/java/com/cognitect/transit/impl/Constants.java
@@ -0,0 +1,12 @@
+package com.cognitect.transit.impl;
+
+public final class Constants {
+    public static final char ESC = '~';
+    public static final String ESC_STR = String.valueOf(ESC);
+    public static final char TAG = '#';
+    public static final String TAG_STR = String.valueOf(TAG);
+    public static final char SUB = '^';
+    public static final String SUB_STR = String.valueOf(SUB);
+    public static final char RESERVED = '`';
+    public static final String ESC_TAG = String.valueOf(ESC) + TAG;
+}
