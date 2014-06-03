e83c8a49e6ebe79e8d8e09b1b45cf5808552ad95
fogus <mefogus@gmail.com>
2014-06-03 11:54:09 -0400

    Placeholder for machine-mode tests.

---
+++ src/test/java/com/cognitect/transit/TransitJSONMachineModeTest.java
@@ -0,0 +1,117 @@
+// Copyright (c) Cognitect, Inc.
+// All rights reserved.
+
+package com.cognitect.transit;
+
+import com.cognitect.transit.impl.AbstractParser;
+import com.cognitect.transit.impl.JsonParser;
+import com.cognitect.transit.impl.ReadCache;
+import com.cognitect.transit.impl.WriteCache;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.apache.commons.codec.binary.Base64;
+
+import java.io.*;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class TransitJSONMachineModeTest extends TestCase {
+
+    public TransitJSONMachineModeTest(String testName) {
+        super(testName);
+    }
+
+    public static Test suite() {
+        return new TestSuite(TransitJSONMachineModeTest.class);
+    }
+
+    // Reading
+
+    public Reader reader(String s) throws IOException {
+
+        InputStream in = new ByteArrayInputStream(s.getBytes());
+        return TransitFactory.reader(TransitFactory.Format.JSON, in, null);
+    }
+
+    private long readTimeString(String timeString) throws IOException {
+        // TODO: Impl date format
+        return 0;
+    }
+
+    private SimpleDateFormat formatter(String formatString) {
+
+        SimpleDateFormat df = new SimpleDateFormat(formatString);
+        df.setTimeZone(TimeZone.getTimeZone("UTC"));
+        return df;
+    }
+
+    public void testReadTime() throws Exception {
+        // TODO: Impl date format
+    }
+
+    public void testReadMap() throws IOException {
+        // TODO: Impl machine format read mode
+    }
+
+    public void testReadMapWithNested() throws IOException {
+        // TODO: Impl machine format read mode
+    }
+
+    // Writing
+
+    public String write(Object o) throws Exception {
+
+        OutputStream out = new ByteArrayOutputStream();
+        Writer w = TransitFactory.writer(TransitFactory.Format.JSON_MACHINE, out, null);
+        w.write(o);
+        return out.toString();
+
+    }
+
+    public boolean isEqual(Object o1, Object o2) {
+
+        if(o1 instanceof Boolean)
+            return o1 == o2;
+        else
+            return false;
+    }
+
+    public void testRoundTrip() throws Exception {
+
+        Object inObject = true;
+
+        OutputStream out = new ByteArrayOutputStream();
+        Writer w = TransitFactory.writer(TransitFactory.Format.JSON_MACHINE, out, null);
+        w.write(inObject);
+        String s = out.toString();
+        InputStream in = new ByteArrayInputStream(s.getBytes());
+        Reader reader = TransitFactory.reader(TransitFactory.Format.JSON, in, null);
+        Object outObject = reader.read();
+
+        assertTrue(isEqual(inObject, outObject));
+    }
+
+    public String scalar(String value) {
+        return "{\"~#'\":"+value+"}";
+    }
+
+    public void testWriteMap() throws Exception {
+
+        Map m = new HashMap();
+        m.put("foo", 1);
+        m.put("bar", 2);
+
+        assertEquals("{\"foo\":1,\"bar\":2}", write(m));
+    }
+
+    public void testWriteEmptyMap() throws Exception {
+
+        Map m = new HashMap();
+        assertEquals("{}", write(m));
+    }
+}
