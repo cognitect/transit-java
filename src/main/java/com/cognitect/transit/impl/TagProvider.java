2d726223d7b41c3f774d4ba8602e29a82ba37616
David Chelimsky <pair+dc@cognitect.com>
2015-01-18 21:21:22 -0200

    correct copyright notice

--- src/main/java/com/cognitect/transit/impl/TagProvider.java
+++ src/main/java/com/cognitect/transit/impl/TagProvider.java
@@ -1,8 +1,8 @@
+// Copyright (c) Cognitect, Inc.
+// All rights reserved.
+
 package com.cognitect.transit.impl;
 
-/**
- * Created by david on 1/18/15.
- */
 public interface TagProvider {
     public String getTag(Object o);
 }
