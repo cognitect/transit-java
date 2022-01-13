d0c7710689122de811bee2add519d7ecf3d49c0e
Alex Miller <alex.miller@cognitect.com>
2022-01-13 13:37:35 -0600

    just compile, no need for jar

--- build.clj
+++ build.clj
@@ -1,4 +1,5 @@
 (ns build
+  (:refer-clojure :exclude [compile]
   (:require [clojure.tools.build.api :as b]))
 
 (def basis (b/create-basis nil))
@@ -6,8 +7,7 @@
 (def version (str (slurp "VERSION_PREFIX") "." (b/git-count-revs nil)))
 (def class-dir "target/classes")
 
-(defn jar
+(defn compile
   [_]
   (b/delete {:path "target"})
-  (b/javac {:src-dirs ["src/main/java"] :class-dir class-dir :basis basis})
-  (b/jar {:class-dir class-dir :jar-file (format "target/tools.build-%s.jar" version)}))
+  (b/javac {:src-dirs ["src/main/java"] :class-dir class-dir :basis basis}))
